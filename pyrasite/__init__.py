308d5c8c742eba592353ab44583835c104551821
Luke Macken <lmacken@redhat.com>
2012-02-11 19:53:40 -0500

    Add pyrasite.__license__

--- pyrasite/__init__.py
+++ pyrasite/__init__.py
@@ -1,4 +1,19 @@
 from inject import CodeInjector
 from inspect import ObjectInspector
+
 __version__ = '1.1'
 __all__ = ('CodeInjector', 'ObjectInspector')
+__license__ = """\
+pyrasite is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pyrasite is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyrasite.  If not, see <http://www.gnu.org/licenses/>.\
+"""

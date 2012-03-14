cb0d8a43d2303a419223c57989dc7966b96f4fa6
Luke Macken <lmacken@redhat.com>
2012-03-14 16:34:25 -0700

    beta5

--- pyrasite/__init__.py
+++ pyrasite/__init__.py
@@ -3,7 +3,7 @@ from .inspect import ObjectInspector
 from .ipc import PyrasiteIPC
 from .reverse import ReverseConnection, ReversePythonConnection
 
-__version__ = '2.0beta4'
+__version__ = '2.0beta5'
 __all__ = ('CodeInjector', 'ObjectInspector', 'PyrasiteIPC',
            'ReverseConnection', 'ReversePythonConnection')
 __license__ = """\

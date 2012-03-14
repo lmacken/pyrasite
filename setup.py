1378dd0beb4d06ecf663aa9b9ed5acbaa50dce79
Luke Macken <lmacken@redhat.com>
2012-03-14 16:55:26 -0700

    remove the pyrasite-gui from our console_scripts

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(name='pyrasite',
       entry_points="""
           [console_scripts]
           pyrasite = pyrasite.main:main
-          pyrasite-gui = pyrasite.tools.gui:main
           pyrasite-memory-viewer = pyrasite.tools.memory_viewer:main
       """,
       classifiers=[

a5df282c49967becfc8fd756d9969bbde422bbc3
Luke Macken <lmacken@redhat.com>
2013-06-21 01:33:00 -0400

    Use the new io.StringIO in python3

--- pyrasite/tests/test_cli.py
+++ pyrasite/tests/test_cli.py
@@ -18,7 +18,10 @@
 import os
 import sys
 
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 from pyrasite.main import main
 
